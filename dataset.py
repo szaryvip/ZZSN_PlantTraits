2e2f6a2ca96d7d4fc6157a8881374b89dae6d647
Łukasz Szarejko <lukaszszarejko@wp.pl>
2024-05-08 19:15:30 +0200

    Add denormalizing and saving predicitons

--- dataset.py
+++ dataset.py
@@ -7,7 +7,8 @@ class PGLSDataset(Dataset):
         """
         Args:
             csv_file (string): Path to the CSV file containing data.
-            transform (callable, optional): Optional transform to be applied on a sample.
+            transform (callable, optional): Optional transform
+                to be applied on a sample.
         """
         self.data_frame = tabular_data
         self.image_folder = image_folder
@@ -23,16 +24,29 @@ class PGLSDataset(Dataset):
 
         sample = self.data_frame.iloc[idx]
         id = int(sample['id'])
-        image = self.image_folder.loader(self.image_folder.root + "/0/" + str(id) + ".jpeg")
+        image = self.image_folder.loader(self.image_folder.root + "/0/" +
+                                         str(id) + ".jpeg")
 
         if self.image_folder.transform is not None:
             image = self.image_folder.transform(image)
 
-        targets = sample[[f"{target}_mean" for target in self.targets]].values
-        features = sample.drop(["id"] + [f"{target}_mean" for target in self.targets] + [f"{target}_sd" for target in self.targets])
+        try:
+            targets =\
+                sample[[f"{target}_mean" for target in self.targets]].values
+        except KeyError:
+            targets = None
+
+        features = sample.drop(["id"])
+        if targets is not None:
+            features =\
+                features.drop([f"{target}_mean" for target in self.targets] +
+                              [f"{target}_sd" for target in self.targets])
 
         features = torch.tensor(features.values, dtype=torch.float32)
-        targets = torch.tensor(targets, dtype=torch.float32)
+        if targets is not None:
+            targets = torch.tensor(targets, dtype=torch.float32)
+        else:
+            targets = torch.tensor([0]*6, dtype=torch.float32)
 
         if self.transform_csv:
             features = self.transform_csv(features)
