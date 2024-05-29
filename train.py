1145866f08f5f20be98a5ebf65bb6fec275364b6
Piotr Gręda <01158700@pw.edu.pl>
2024-05-29 12:24:25 +0200

    Changed metrics.reset placement

--- train.py
+++ train.py
@@ -11,11 +11,9 @@ def denormalize_targets(targets, original_means, original_stds):
 
 def validate_model(model, metric, val_data_loader,
                    original_means, original_stds):
-    model.eval()
     iterations = 0
     accumulated_r2 = 0
     with torch.no_grad():
-        metric.reset()
         for val_data in val_data_loader:
             image, features, targets = val_data
             image = image.to(device)
@@ -43,11 +41,11 @@ def train_model(model, train_data_loader, val_data_loader, model_path_prefix,
 
     optimizer = torch.optim.Adam(model.parameters(), maximize=False, lr=1e-4,
                                  weight_decay=1e-5)
-    max_value_metrics_batch = float("-inf")
     model_best_batch_path = f"./models/{model_path_prefix}_best_batch.pt"
     max_value_metrics_epoch = float("-inf")
     model_best_epoch_path = f"./models/{model_path_prefix}_best_epoch.pt"
     model_last_path = f"./models/{model_path_prefix}_last.pt"
+    metrics_file = f"./models/metrics_{model_path_prefix}.txt"
     last_value_metrics_validation = float("-inf")
     batch_iteration = 0
     validation_decrease_counter = 0
@@ -73,25 +71,24 @@ def train_model(model, train_data_loader, val_data_loader, model_path_prefix,
                                                  original_stds)
             metric.update(outputs_denorm, targets_denorm)
             new_max_value_metrics_batch = metric.compute().item()
-            with open("metrics.txt", "a") as f:
+            with open(metrics_file, "a") as f:
                 f.write(f"Epoch {epoch}: R2={new_max_value_metrics_batch}\n")
 
-            if (new_max_value_metrics_batch > max_value_metrics_batch):
-                max_value_metrics_batch = new_max_value_metrics_batch
-                torch.save(model.state_dict(), model_best_batch_path)
-
             if batch_iteration % validation_after_n_batches == 0:
+                model.eval()
                 new_validation_r2 = validate_model(model, metric,
                                                    val_data_loader,
                                                    original_means,
                                                    original_stds)
-                with open("metrics.txt", "a") as f:
+                metric.reset()
+                with open(metrics_file, "a") as f:
                     f.write(f"Validation R2={new_validation_r2}\n")
                 if new_validation_r2 < last_value_metrics_validation:
                     validation_decrease_counter += 1
                     if validation_decrease_counter == 5:
                         exit(0)
                 else:
+                    torch.save(model.state_dict(), model_best_batch_path)
                     last_value_metrics_validation = new_validation_r2
                     validation_decrease_counter = 0
                 model.train()
